f270bfdc98278b55702637502fd42079a347c765
saurabhpojhi <saurabh.kumarjan10@gmail.com>
2022-06-14 23:59:22 +0530

    aggregation

--- Aggregation_Task.js
+++ Aggregation_Task.js
@@ -1,1 +1,14 @@
 // join two or more colection using lookup
+
+/*
+{
+    $lookup:
+    {
+        from: < "from collection" >,  
+        localField: < any field from "input collection" >,
+        foreignField: < any field from "from collection" >,
+        as: < attached array field >
+    }
+}
+
+*/
